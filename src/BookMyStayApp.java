29a73fe935d01a5be1111c8d32c7370720d1d96e
shyamsiddhartha27 <shyamsiddharthagorle7@gmail.com>
2026-03-12 09:54:49 +0530

    Base application setup

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,2 @@
 public class BookMyStayApp {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Hotel Booking Management System");
-            System.out.println("Application: Hotel Booking System v1.0");
-        }
 }
