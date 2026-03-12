068f402c4cb09b651aec0cc481e31b9b0ab52779
shyamsiddhartha27 <shyamsiddharthagorle7@gmail.com>
2026-03-12 10:26:47 +0530

    UC3: Implement centralized room inventory using HashMap

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BookMyStayApp {
 
     static abstract class Room {
@@ -25,6 +28,70 @@ public class BookMyStayApp {
     }
 
     static class DoubleRoom extends Room {
+        public DoubleRoom() {
+            super(2, 400, 2500.0);
+        }
+    }
+
+    static class SuiteRoom extends Room {
+        public SuiteRoom() {
+            super(3, 750, 5000.0);
+        }
+    }
+
+    static class RoomInventory {
+
+        private Map<String, Integer> roomAvailability;
+
+        public RoomInventory() {
+            roomAvailability = new HashMap<>();
+            initializeInventory();
+        }
+
+        private void initializeInventory() {
+            roomAvailability.put("Single", 5);
+            roomAvailability.put("Double", 3);
+            roomAvailability.put("Suite", 2);
+        }
+
+        public Map<String, Integer> getRoomAvailability() {
+            return roomAvailability;
+        }
+
+        public void updateAvailability(String roomType, int count) {
+            roomAvailability.put(roomType, count);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Inventory Status\n");
+
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        RoomInventory inventory = new RoomInventory();
+
+        System.out.println("Single Room:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " +
+                inventory.getRoomAvailability().get("Single") + "\n");
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " +
+                inventory.getRoomAvailability().get("Double") + "\n");
+
+        System.out.println("Suite Room:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " +
+                inventory.getRoomAvailability().get("Suite"));
+    }
+}
+
+
+static class DoubleRoom extends Room {
         public DoubleRoom() {
             super(2, 400, 2500.0);
         }
